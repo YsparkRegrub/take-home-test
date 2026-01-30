abf6ba7a854e354ef8c02cd67af7cc15fa1d203c
KrapsyBurger <nathanfascia@gmail.com>
2026-01-30 11:26:38 +0100

    rename test

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -7,19 +7,19 @@ describe("Pharmacy", () => {
 
 
   describe ("Global cases", () => {
-    it('should decreases benefit and expiresIn by 1', () => {
+    it('decreases benefit and expiresIn by 1', () => {
       expect(updateOnce(new Drug(DrugName.DOLIPRANE, 5, 10))).toEqual(
         new Drug(DrugName.DOLIPRANE, 4, 9),
       );
     })
 
-    it('should decreases benefit twice as fast after expiration date', () => {
+    it('decreases benefit twice as fast after expiration', () => {
       expect(updateOnce(new Drug(DrugName.DOLIPRANE, 0, 10))).toEqual(
         new Drug(DrugName.DOLIPRANE, -1, 8),
       );
     })
 
-    it('should not decreases benefit below 0', () => {
+    it('floor benefit to 0', () => {
       expect(updateOnce(new Drug(DrugName.DOLIPRANE, 5, 0))).toEqual(
         new Drug(DrugName.DOLIPRANE, 4, 0),
       );
@@ -28,19 +28,19 @@ describe("Pharmacy", () => {
 
   describe("Specific cases", () => {
     describe("Herbal Tea", () => {
-      it("should increase benefit by 1 before expiration", () => {
+      it("increases benefit by 1 before expiration", () => {
             expect(updateOnce(new Drug(DrugName.HERBAL_TEA, 5, 10))).toEqual(
             new Drug(DrugName.HERBAL_TEA, 4, 11),
           );
       })
 
-      it("should increase benefit by 2 after expiration", () => {
+      it("increases benefit by 2 after expiration", () => {
         expect(updateOnce(new Drug(DrugName.HERBAL_TEA, 0, 10))).toEqual(
           new Drug(DrugName.HERBAL_TEA, -1, 12),
         );
       })
 
-      it("should not increase benefit above 50", () => {
+      it("benefit capped at 50", () => {
         expect(updateOnce(new Drug(DrugName.HERBAL_TEA, 5, 50))).toEqual(
           new Drug(DrugName.HERBAL_TEA, 4, 50),
         );
@@ -48,7 +48,7 @@ describe("Pharmacy", () => {
     })
 
     describe("Magic Pill", () => {
-      it("should not change benefit or expiresIn", () => {
+      it("does not change benefit or expiration date", () => {
         expect(updateOnce(new Drug(DrugName.MAGIC_PILL, 5, 40))).toEqual(
           new Drug(DrugName.MAGIC_PILL, 5, 40),
         );
@@ -56,25 +56,25 @@ describe("Pharmacy", () => {
     })
 
     describe("Fervex", () => {
-      it("should increase benefit by 1 when more than 10 days left", () => {
+      it("increases benefit by 1 when 10+ days left", () => {
         expect(updateOnce(new Drug(DrugName.FERVEX, 15, 10))).toEqual(
           new Drug(DrugName.FERVEX, 14, 11),
         );
       })
 
-      it("should increase benefit by 2 when 10 days or less", () => {
+      it("increases benefit by 2 when 10 days or less", () => {
         expect(updateOnce(new Drug(DrugName.FERVEX, 10, 10))).toEqual(
           new Drug(DrugName.FERVEX, 9, 12),
         );
       })
 
-      it("should increase benefit by 3 when 5 days or less", () => {
+      it("increases benefit by 3 when 5 days or less", () => {
         expect(updateOnce(new Drug(DrugName.FERVEX, 5, 10))).toEqual(
           new Drug(DrugName.FERVEX, 4, 13),
         );
       })
 
-      it("should drop benefit to 0 after expiration", () => {
+      it("drops benefit to 0 after expiration", () => {
         expect(updateOnce(new Drug(DrugName.FERVEX, 0, 10))).toEqual(
           new Drug(DrugName.FERVEX, -1, 0),
         );
@@ -82,19 +82,19 @@ describe("Pharmacy", () => {
     })
 
     describe("Dafalgan", () => {
-      it("should decrease benefit by 2 before expiration", () => {
+      it("decreases benefit by 2 before expiration", () => {
         expect(updateOnce(new Drug(DrugName.DAFALGAN, 5, 10))).toEqual(
           new Drug(DrugName.DAFALGAN, 4, 8),
         );
       })
 
-      it("should decrease benefit by 4 after expiration", () => {
+      it("decreases benefit by 4 after expiration", () => {
         expect(updateOnce(new Drug(DrugName.DAFALGAN, 0, 10))).toEqual(
           new Drug(DrugName.DAFALGAN, -1, 6),
         );
       })
 
-      it("should not decrease benefit below 0", () => {
+      it("floors benefit to 0", () => {
         expect(updateOnce(new Drug(DrugName.DAFALGAN, 5, 1))).toEqual(
           new Drug(DrugName.DAFALGAN, 4, 0),
         );
